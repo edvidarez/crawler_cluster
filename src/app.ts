268772dcc616bf12c11049ed11a22793e4018c7a
edvidarez <edvidarez@pop-os.localdomain>
2022-05-01 18:56:10 -0500

    changed default port

--- src/app.ts
+++ src/app.ts
@@ -15,9 +15,9 @@ const start = () => {
     const html = await crawl(req.body);
     res.json({ html });
   });
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8080;
   app.listen(port, () => {
-    console.log("server started on port 3000");
+    console.log("server started on port", port);
   });
 };
 start();
